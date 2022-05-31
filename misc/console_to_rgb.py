1e7d79831613a41b25ac4fe7d40f43bd4a75d8ff
joric <joric@users.noreply.github.com>
2022-05-31 10:11:00 +0500

    added console_to_rgb.py

---
+++ misc/console_to_rgb.py
@@ -0,0 +1,13 @@
+import re
+
+c={}
+
+for s in open('conemu-colors.xml').read().splitlines():
+    if m:=re.match(r'.*ColorTable(\d\d).*00(\w\w)(\w\w)(\w\w)',s):
+        c['#%X' % int(m[1])] = '#'+m[4]+m[3]+m[2]
+
+out = open('../hrd/monokai-rgb.hrd','w')
+
+for s in open('../hrd/monokai.hrd').read().splitlines(True):
+    out.write(re.sub(r'(#\w)', lambda x:c.get(x.group(0),'#0'), s))
+
