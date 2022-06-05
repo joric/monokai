e36110e0b5df3fd6351c66915949a313c935d5e3
joric <joric@users.noreply.github.com>
2022-06-05 21:29:46 +0500

    better scripts

--- misc/conemu_to_rgb.py
+++ misc/conemu_to_rgb.py
@@ -1,20 +1,18 @@
 import os, re
+import xml.etree.ElementTree as ET
 
-for s in open(os.environ['FARHOME'] + '\\ConEmu.xml').read().splitlines(True):
-    if "joric's monokai" in s:
-        out = open('conemu-colors.xml','w')
-    if 'out' not in locals():
-        continue
-    if m:=re.match(r'.*ColorTable(\d\d).*00(\w\w)(\w\w)(\w\w)',s):
-        out.write(s)
+colors_filename = "./conemu-colors.xml"
+
+tree = ET.parse(os.environ['FARHOME'] + '\\ConEmu.xml')
+elem = tree.getroot().find("./key/key/key/key/value/[@data='Joric Monokai']/..")
+open(colors_filename,'wb').write(ET.tostring(elem))
 
 c={}
-for s in open('conemu-colors.xml').read().splitlines():
+for s in open(colors_filename).read().splitlines():
     if m:=re.match(r'.*ColorTable(\d\d).*00(\w\w)(\w\w)(\w\w)',s):
         c['#%X' % int(m[1])] = '#'+m[4]+m[3]+m[2]
 
 out = open('../hrd/monokai-rgb.hrd','w')
-
 for s in open('../hrd/monokai.hrd').read().splitlines(True):
     out.write(re.sub(r'(#\w)', lambda x:c.get(x.group(0).upper(),'#0'), s))
 
