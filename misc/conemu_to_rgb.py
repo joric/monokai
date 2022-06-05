273797735ef4870ef4b9eb10e44e6b03df44145d
joric <joric@users.noreply.github.com>
2022-06-05 17:52:12 +0500

    single script

--- misc/conemu_to_rgb.py
+++ misc/conemu_to_rgb.py
@@ -1,7 +1,14 @@
-import re
+import os, re
+
+for s in open(os.environ['FARHOME'] + '\\ConEmu.xml').read().splitlines(True):
+    if "joric's monokai" in s:
+        out = open('conemu-colors.xml','w')
+    if 'out' not in locals():
+        continue
+    if m:=re.match(r'.*ColorTable(\d\d).*00(\w\w)(\w\w)(\w\w)',s):
+        out.write(s)
 
 c={}
-
 for s in open('conemu-colors.xml').read().splitlines():
     if m:=re.match(r'.*ColorTable(\d\d).*00(\w\w)(\w\w)(\w\w)',s):
         c['#%X' % int(m[1])] = '#'+m[4]+m[3]+m[2]
--- misc/export_conemu.py
+++
@@ -1,9 +0,0 @@
-import os, re
-
-for s in open(os.environ['FARHOME'] + '\\ConEmu.xml').read().splitlines(True):
-    if "joric's monokai" in s:
-        out = open('conemu-colors.xml','w')
-    if 'out' not in locals():
-        continue
-    if m:=re.match(r'.*ColorTable(\d\d).*00(\w\w)(\w\w)(\w\w)',s):
-        out.write(s)
