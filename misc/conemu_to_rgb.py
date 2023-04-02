838a5d9a678bfaa2fd2662993490b8e04689e0cd
joric <joric@users.noreply.github.com>
2023-04-02 09:27:39 +0500

    updated files

--- misc/conemu_to_rgb.py
+++ misc/conemu_to_rgb.py
@@ -19,6 +19,6 @@ for s in open(colors_filename).read().splitlines():
     if m:=re.match(r'.*ColorTable(\d\d).*00(\w\w)(\w\w)(\w\w)',s):
         c['#%X' % int(m[1])] = '#'+m[4]+m[3]+m[2]
 
-out = open('../hrd/monokai-rgb.hrd','w')
-for s in open('../hrd/monokai.hrd').read().splitlines(True):
+out = open('../monokai/monokai-rgb.hrd','w')
+for s in open('../monokai/monokai.hrd').read().splitlines(True):
     out.write(re.sub(r'(#\w)', lambda x:c.get(x.group(0).upper(),'#0'), s))
